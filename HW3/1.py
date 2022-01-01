bad3faf5e606e6c32e06bec01f50a660d01d4b65
Vlad <2007vladvv@gmail.com>
2022-01-02 00:10:32 +0300

    add HW3

---
+++ HW3/1.py
@@ -0,0 +1,7 @@
+#  первый способ
+Turtle = "ttttuuuurrrrttttlllleeee"
+print(f"{Turtle[0]}" + f"{Turtle[4]}" + f"{Turtle[8]}" + f"{Turtle[12]}" + f"{Turtle[16]}" + f"{Turtle[20]}")
+
+#  второй способ
+#  помощью [: ] можно делать срез строк
+print(Turtle[3:5] + Turtle[11:13] + Turtle[19:21])
