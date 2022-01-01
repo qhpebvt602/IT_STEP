a9d85fbf7c8a313124bb463cc8f3ec9b80c40c38
Vlad <2007vladvv@gmail.com>
2022-01-01 23:58:06 +0300

    add HW1

---
+++ HW 1/1.py
@@ -0,0 +1,7 @@
+# input - вывод данных
+
+name = input('Введите имя: ')
+age = input('Введите возраст: ')
+phone = input('Введите телефон: ')
+
+print(f"name - {name}, age - {age}, phone - {phone}")
