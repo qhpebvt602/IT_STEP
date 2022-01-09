0ea4b4f740f172ebe75188afcdb736de8b70b503
Vlad <2007vladvv@gmail.com>
2022-01-09 22:07:36 +0300

    первая дз

---
+++ HW6/1.py
@@ -0,0 +1,12 @@
+# database словарь в словаре
+
+database = {}
+print('add your name and tree thing that you take on uninhabited island')
+
+database1 = {input('name:'): {input('1thing:'): {input('2thing:'): {input('3thing:')}}}}
+
+database2 = {input('name:'): {input('1thing:'): {input('2thing:'): {input('3thing:')}}}}
+database3 = {input('name:'): {input('1thing:'): {input('2thing:'): {input('3thing:')}}}}
+print(database1)
+print(database2)
+print(database3)
