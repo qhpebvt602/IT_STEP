37cadc6a5829251d974b9793e4abc7feaa23a12a
Vlad <2007vladvv@gmail.com>
2022-01-10 21:01:36 +0300

    немного поменял

--- HW6/1.py
+++ HW6/1.py
@@ -1,12 +1,12 @@
 # database словарь в словаре
 
-database = {}
 print('add your name and tree thing that you take on uninhabited island')
 
-database1 = {input('name:'): {input('1thing:'): {input('2thing:'): {input('3thing:')}}}}
+database1 = {input('name:'):  {'thing1 ':  input('1thing:'), 'thing2 ':  input('2thing:'),  'thing3 ':  input('3thing:')}}
 
-database2 = {input('name:'): {input('1thing:'): {input('2thing:'): {input('3thing:')}}}}
-database3 = {input('name:'): {input('1thing:'): {input('2thing:'): {input('3thing:')}}}}
+database2 = {input('name:'):  {'thing1 ':  input('1thing:'), 'thing2 ':  input('2thing:'),  'thing3 ':  input('3thing:')}}
+database3 = {input('name:'):  {'thing1 ':  input('1thing:'), 'thing2 ':  input('2thing:'),  'thing3 ':  input('3thing:')}}
+
 print(database1)
 print(database2)
 print(database3)
